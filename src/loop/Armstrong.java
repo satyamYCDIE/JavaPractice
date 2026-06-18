405fdfb9c494ea8c3186db9e2685943ba6f43345
satyamYCDIE <satyam20sarha@gmail.com>
2026-06-18 21:35:08 +0530

    updated

--- src/loop/Armstrong.java
+++ src/loop/Armstrong.java
@@ -5,6 +5,7 @@ public class Armstrong {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Armstrong Number");
+		System.out.println("Armstrong Number");
 
 	}
 
