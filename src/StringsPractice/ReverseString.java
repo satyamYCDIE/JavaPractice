0339ee29a3a65c5485df5ae82ed222b6591c8557
satyamYCDIE <satyam20sarha@gmail.com>
2026-06-22 21:56:53 +0530

    .

--- src/StringsPractice/ReverseString.java
+++ src/StringsPractice/ReverseString.java
@@ -9,12 +9,22 @@ public class ReverseString {
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter the word you want to reverse:");
 		String word=sc.next();
-		int length = word.length();
+		//Approach1......
+		/*int length = word.length();
 		System.out.println(length);
 		String rev="";
 		for(int i=length-1;i>=0;i--)
 		{
 			rev=rev+word.charAt(i);
+		}*/
+		
+		//Approach2.......
+		char a[]=word.toCharArray();
+		String rev="";
+		
+		for(int i=a.length-1;i>=0;i--)
+		{
+			rev=rev+a[i];
 		}
 		System.out.print(rev);
 		
