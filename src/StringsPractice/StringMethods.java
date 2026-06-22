fc41670b006a2e880760405a64e6f414e827d1a8
satyamYCDIE <satyam20sarha@gmail.com>
2026-06-22 20:04:40 +0530

    update

---
+++ src/StringsPractice/StringMethods.java
@@ -0,0 +1,19 @@
+package StringsPractice;
+
+public class StringMethods {
+
+	public static void main(String[] args) 
+	{
+		String s="Welcome";
+		System.out.println(s.length());
+		
+		String s1="Welcome ";
+		String s2="to java ";
+		String s3="Automation";
+		
+		
+		
+		System.out.println(s1.concat(s2).concat(s3));
+	}
+
+}
