e332a9c1f68529bdddf5802f7f63d9d17873a9c3
satyamYCDIE <satyam20sarha@gmail.com>
2026-06-24 21:49:50 +0530

    .

---
+++ src/OOPsConcept/Employee.java
@@ -0,0 +1,35 @@
+package OOPsConcept;
+
+public class Employee {
+
+	int eid;
+	String ename;
+	String job;
+	int sal;
+	
+	void display()
+	{
+		System.out.println(eid);
+		System.out.println(ename);
+		System.out.println(job);
+		System.out.println(sal);
+	}
+	
+	public static void main(String[] args) 
+	{
+		Employee emp1=new Employee();
+		emp1.eid=101;
+		emp1.ename="Satyam";
+		emp1.job="Software Engineer";
+		emp1.sal=50000;
+		emp1.display();
+		
+		Employee emp2=new Employee();
+		emp2.eid=102;
+		emp2.ename="Vishakha";
+		emp2.job="Manager";
+		emp2.sal=100000;
+		emp2.display();
+	}
+
+}
